822e33015df29dba01ffabe81b02382d214aa826
Adam Byrd <ajbyrd0723@gmail.com>
2020-02-21 11:12:13 -0600

    started shopping cart view

---
+++ bangazon-client-facing/src/components/ShoppingCartProduct.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+
+
+class ShoppingCartProduct extends Component {
+
+    render() {
+        return (
+            <>
+                <section>
+                    <p>
+                        <span>
+                            {this.props.}
+                        </span>
+                    </p>
+                </section>
+
+            </>
+        )
+    }
+}
