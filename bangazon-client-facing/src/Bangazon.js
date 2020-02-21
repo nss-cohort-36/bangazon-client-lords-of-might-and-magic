6d75fa9a7b58586d815af98315e612d0570206d2
Sullivan Pierce <sullypierce@gmail.com>
2020-02-21 13:17:58 -0600

    working products list

--- bangazon-client-facing/src/Bangazon.js
+++ bangazon-client-facing/src/Bangazon.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Bangazon.css';
 import NavBar from './components/nav/NavBar'
+import ApplicationViews from './components/ApplicationViews';
 
 class Bangazon extends Component {
 
   render() {
     return (
+      <>
       <NavBar />
+      <ApplicationViews/>
+      </>
     )
   }
 
