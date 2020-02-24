d93eeb8660fe0070d2eb68d9f8079e8f9df20671
Sullivan Pierce <sullypierce@gmail.com>
2020-02-24 14:08:25 -0600

    good

--- bangazon-client-facing/src/components/product/ProductCard.js
+++ bangazon-client-facing/src/components/product/ProductCard.js
@@ -12,6 +12,7 @@ class ProductCard extends Component {
           <p>Cost: ${this.props.product.price}</p>
           <div>Seller Description:</div>
           <div>{this.props.product.description}</div>
+          <button>Add to Cart</button>
         </section>
       </>
     )
