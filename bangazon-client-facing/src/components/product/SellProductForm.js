b34a47609c3d41884caa448beae3b3450cef88e2
Chase Fite <chasef7031@gmail.com>
2020-02-24 13:26:56 -0600

    implemented sell product form

--- bangazon-client-facing/src/components/product/SellProductForm.js
+++ bangazon-client-facing/src/components/product/SellProductForm.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import ApiManager from '../utility/ApiManager'
 
 class SellProductForm extends Component {
 
-    // need to set customer id in state to whatever credentials in session storage says
     state = {
         name: "",
-        customer_id: 1,
         price: 0,
         description: "",
         quantity: 0,
@@ -15,14 +14,30 @@ class SellProductForm extends Component {
         localDeliveryAvailable: false,
     }
 
-    componentDidMount() {
-
-    }
-
     handleFieldChange = evt => {
         const stateToChange = {}
         stateToChange[evt.target.id] = evt.target.value
         this.setState(stateToChange)
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        const newProduct = {
+            name: this.state.name,
+            price: this.state.price,
+            description: this.state.description,
+            quantity: this.state.quantity,
+            location: this.state.location,
+            image_path: this.state.imagePath,
+            product_type_id: this.state.productTypeId
+        }
+
+        ApiManager.post('products', newProduct)
+        .then(() => {
+            this.props.history.push('/')
+        })
+
     }
 
     render() {
@@ -32,19 +47,19 @@ class SellProductForm extends Component {
                 <form className="pa4 black-80">
                     <div className="measure">
                         <label for="name" className="f6 b db mb2">Name</label>
-                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
                         <label for="price" className="f6 b db mb2">Price</label>
-                        <input id="price" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                        <input id="price" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
                         <label for="description" className="f6 b db mb2">Description</label>
-                        <input id="description" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                        <input id="description" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
                         <label for="quantity" className="f6 b db mb2">Quantity</label>
-                        <input id="quantity" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                        <input id="quantity" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
                         <label for="location" className="f6 b db mb2">City</label>
@@ -52,22 +67,22 @@ class SellProductForm extends Component {
                     </div>
                     <div className="flex items-center mb2">
                         <label for="localDeliveryAvailable" className="lh-copy lda-label">Local Delivery Available</label>
-                        <input className="mr2" type="checkbox" id="localDeliveryAvailable" onChange={this.handleFieldChange} />
+                        <input className="mr2" type="checkbox" id="localDeliveryAvailable" />
                     </div>
                     <div className="measure">
                         <label for="imagePath" className="f6 b db mb2">Image Path</label>
-                        <input id="imagePath" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                        <input id="imagePath" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
                         <label className="f6 b db mb2">Type</label>
-                        <input list="productTypeList" name="productTypeId" className="input-reset ba b--black-20 pa2 mb2 db w-100 product-types-input" onChange={this.handleFieldChange} id="productTypeId" />
+                        <input list="productTypeList" name="productTypeId" className="input-reset ba b--black-20 pa2 mb2 db w-100 product-types-input" onChange={this.handleFieldChange} id="productTypeId" required />
                         <datalist id="productTypeList">
                             <option value={1}>Product type 1</option>
                             <option value={2}>Product type 2</option>
                             <option value={3}>Product type 3</option>
                         </datalist>
                     </div>
-                    <a class="f6 link dim ba ph3 pv2 mb2 dib black" href="#0">Submit</a>
+                    <button onClick={this.handleSubmit}>Submit</button>
                 </form>
             </>
         )
