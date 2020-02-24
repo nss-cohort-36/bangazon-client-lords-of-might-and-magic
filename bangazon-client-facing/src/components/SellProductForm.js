6aab86638d15f4fa484babf8736483bc48695ff0
Chase Fite <chasef7031@gmail.com>
2020-02-24 10:42:01 -0600

    pr

--- bangazon-client-facing/src/components/SellProductForm.js
+++ bangazon-client-facing/src/components/SellProductForm.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 
 class SellProductForm extends Component {
+
+    // need to set customer id in state to whatever credentials in session storage says
     state = {
         name: "",
+        customer_id: 1,
         price: 0,
         description: "",
         quantity: 0,
@@ -11,45 +14,60 @@ class SellProductForm extends Component {
         productTypeId: 0,
         localDeliveryAvailable: false,
     }
-    
+
+    componentDidMount() {
+
+    }
+
+    handleFieldChange = evt => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
     render() {
         return (
             <>
-                <h2 class="f6 gray fw2 ttu tracked product-header">List a Product for Sell</h2>
+                <h2 className="f6 gray fw2 ttu tracked product-header">List a Product for Sell</h2>
                 <form className="pa4 black-80">
                     <div className="measure">
                         <label for="name" className="f6 b db mb2">Name</label>
-                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
+                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                    </div>
+                    <div className="measure">
+                        <label for="price" className="f6 b db mb2">Price</label>
+                        <input id="price" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                    </div>
+                    <div className="measure">
+                        <label for="description" className="f6 b db mb2">Description</label>
+                        <input id="description" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                    </div>
+                    <div className="measure">
+                        <label for="quantity" className="f6 b db mb2">Quantity</label>
+                        <input id="quantity" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                    </div>
+                    <div className="measure">
+                        <label for="location" className="f6 b db mb2">City</label>
+                        <input id="location" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
+                    </div>
+                    <div className="flex items-center mb2">
+                        <label for="localDeliveryAvailable" className="lh-copy lda-label">Local Delivery Available</label>
+                        <input className="mr2" type="checkbox" id="localDeliveryAvailable" onChange={this.handleFieldChange} />
+                    </div>
+                    <div className="measure">
+                        <label for="imagePath" className="f6 b db mb2">Image Path</label>
+                        <input id="imagePath" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} />
                     </div>
                     <div className="measure">
                         <label className="f6 b db mb2">Type</label>
-                        <input list="product-types" name="product-types" className="input-reset ba b--black-20 pa2 mb2 db w-100 product-types-input" />
-                        <datalist id="product-types">
-                            <option value="Product type 1" />
-                            <option value="Product type 2" />
-                            <option value="Product type 3" />
+                        <input list="productTypeList" name="productTypeId" className="input-reset ba b--black-20 pa2 mb2 db w-100 product-types-input" onChange={this.handleFieldChange} id="productTypeId" />
+                        <datalist id="productTypeList">
+                            <option value={1}>Product type 1</option>
+                            <option value={2}>Product type 2</option>
+                            <option value={3}>Product type 3</option>
                         </datalist>
                     </div>
-                    <div className="measure">
-                        <label for="name" className="f6 b db mb2">Quantity</label>
-                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
-                    </div>
-                    <div className="measure">
-                        <label for="name" className="f6 b db mb2">Price</label>
-                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
-                    </div>
-                    <div className="measure">
-                        <label for="name" className="f6 b db mb2">Description</label>
-                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
-                    </div>
-                    <div class="flex items-center mb2">
-                        <label for="local-delivery-available" class="lh-copy lda-label">Local Delivery Available</label>
-                        <input class="mr2" type="checkbox" id="local-delivery-available" />
-                    </div>
-                    <div className="measure">
-                        <label for="name" className="f6 b db mb2">City</label>
-                        <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
-                    </div>
+                    <a class="f6 link dim ba ph3 pv2 mb2 dib black" href="#0">Submit</a>
                 </form>
             </>
         )
