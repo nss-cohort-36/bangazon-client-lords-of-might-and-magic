dcd9ed75a41e18dea9df258a9666b54644a28c38
Manila Bui <manila.bui@gmail.com>
2020-02-25 14:45:29 -0600

    fixed error

--- bangazon-client-facing/src/components/settings/Settings.js
+++ bangazon-client-facing/src/components/settings/Settings.js
@@ -24,12 +24,11 @@ export default class Settings extends Component {
   deletePaymentType = id => {
     fetch(`http://localhost:8000/paymenttypes/${id}`, {
       "method": 'DELETE',
-        "headers": {
-          "Accept": "application/json",
-          "Authorization": `Token ${sessionStorage.getItem("bangazon_token")}`
-        }
+      "headers": {
+        "Accept": "application/json",
+        "Authorization": `Token ${sessionStorage.getItem("bangazon_token")}`
+      }
     })
-    .then(r => r.json());
 
     this.getPaymentTypes();
   }
