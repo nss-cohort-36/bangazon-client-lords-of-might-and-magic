6453e967c8b68af65f1e8ce158640ec5786901ca
Manila Bui <manila.bui@gmail.com>
2020-02-25 10:14:16 -0600

    minor styling

--- bangazon-client-facing/src/components/settings/Settings.js
+++ bangazon-client-facing/src/components/settings/Settings.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { isAuthenticated } from "../helpers/simpleAuth"
 import './Settings.css'
 
 // needs to receive the payment types as an argument
-export default () => {
-	return (
-		<article>
-      <details className="w-70-ns w-100 pt2">
-        <summary className="pointer dim">
-            <h2 className='f5-ns f6 fw3 underline'>
-                Payment Options
-            </h2>
-        </summary>
-        <div className="f6">
-            <a className="pointer dim blue" href='/addpaymenttype' title="Add Payment Type Form">
-              Add a Payment Type
-            </a>
-          	<p>Payment Type 1</p>
-          	<p>Payment Type 2</p>
-        </div>
-      </details>
-	  </article>
-	)
+export default class Settings extends Component {
+  state = {
+      paymentTypes: []
+  }
+
+  getPaymentTypes = () => {
+    if (isAuthenticated())
+        fetch('http://localhost:8000/paymenttypes', {
+            "method": "GET",
+            "headers": {
+                "Accept": "application/json",
+                "Authorization": `Token ${sessionStorage.getItem("bangazon_token")}`
+              }
+        })
+        .then(r => r.json())
+        .then(items => this.setState({paymentTypes: items})) 
+    }
+
+  componentDidMount() {
+    this.getPaymentTypes();
+  }
+
+  render() {
+  	return (
+  		<article>
+        <details className="w-70-ns w-100 pt2">
+          <summary className="pointer dim">
+              <h2 className='f5-ns f6 fw3 underline'>
+                  Payment Options
+              </h2>
+          </summary>
+          <div className="f6">
+              <a className="pointer dim blue" href='/add/paymenttype' title="Add Payment Type Form">
+                Add a Payment Type
+              </a>
+            	{this.state.paymentTypes.map(type => {
+                  console.log(type)
+              })}
+          </div>
+        </details>
+  	  </article>
+  	)
+  }
 }
