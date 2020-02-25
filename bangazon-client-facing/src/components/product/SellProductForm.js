ed07578f8274a63afec08509311fefe6faed9ecc
Sullivan Pierce <sullypierce@gmail.com>
2020-02-25 11:08:01 -0600

    product type can be set in sell product form

--- bangazon-client-facing/src/components/product/SellProductForm.js
+++ bangazon-client-facing/src/components/product/SellProductForm.js
@@ -13,12 +13,18 @@ class SellProductForm extends Component {
         imagePath: "",
         productTypeId: 0,
         localDeliveryAvailable: false,
+        producttypes: []
     }
 
     handleFieldChange = evt => {
         const stateToChange = {}
         stateToChange[evt.target.id] = evt.target.value
         this.setState(stateToChange)
+    }
+
+    componentDidMount() {
+        ApiManager.get('producttypes')
+        .then(producttypes => this.setState({producttypes}))
     }
 
     handleSubmit = event => {
@@ -49,41 +55,42 @@ class SellProductForm extends Component {
                 <h2 className="f6 gray fw2 ttu tracked product-header">List a Product for Sell</h2>
                 <form className="pa4 black-80" onSubmit={this.handleSubmit} >
                     <div className="measure">
-                        <label for="name" className="f6 b db mb2">Name</label>
+                        <label htmlFor="name" className="f6 b db mb2">Name</label>
                         <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
-                        <label for="price" className="f6 b db mb2">Price</label>
+                        <label htmlFor="price" className="f6 b db mb2">Price</label>
                         <input id="price" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
-                        <label for="description" className="f6 b db mb2">Description</label>
+                        <label htmlFor="description" className="f6 b db mb2">Description</label>
                         <input id="description" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
-                        <label for="quantity" className="f6 b db mb2">Quantity</label>
+                        <label htmlFor="quantity" className="f6 b db mb2">Quantity</label>
                         <input id="quantity" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="measure">
-                        <label for="location" className="f6 b db mb2">City</label>
+                        <label htmlFor="location" className="f6 b db mb2">City</label>
                         <input id="location" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required />
                     </div>
                     <div className="flex items-center mb2">
-                        <label for="localDeliveryAvailable" className="lh-copy lda-label">Local Delivery Available</label>
+                        <label htmlFor="localDeliveryAvailable" className="lh-copy lda-label">Local Delivery Available</label>
                         <input className="mr2" type="checkbox" id="localDeliveryAvailable"  />
                     </div>
                     <div className="measure">
-                        <label for="imagePath" className="f6 b db mb2">Image Path</label>
+                        <label htmlFor="imagePath" className="f6 b db mb2">Image Path</label>
                         <input id="imagePath" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" onChange={this.handleFieldChange} required/>
                     </div>
                     <div className="measure">
                         <label className="f6 b db mb2">Type</label>
                         <input list="productTypeList" name="productTypeId" className="input-reset ba b--black-20 pa2 mb2 db w-100 product-types-input" onChange={this.handleFieldChange} id="productTypeId" required />
-                        <datalist id="productTypeList">
-                            <option value={1}>Product type 1</option>
-                            <option value={2}>Product type 2</option>
-                            <option value={3}>Product type 3</option>
-                        </datalist>
+                        <label htmlFor="productTypeList">Product Type</label>
+                        <select id="productTypeList">
+                            {this.state.producttypes.map(type => 
+                                <option key={type.id} value={type.id}>{type.name}</option>
+                            )}
+                        </select>
                     </div>
                     <button>Submit</button>
                 </form>
