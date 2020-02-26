535763f00d4e41ab1d9910929b8b83ff5edb5315
Adam Byrd <ajbyrd0723@gmail.com>
2020-02-26 09:12:18 -0600

    stuff

--- bangazon-client-facing/src/components/Order/OrderProduct.js
+++ bangazon-client-facing/src/components/Order/OrderProduct.js
@@ -6,9 +6,9 @@ class OrderProduct extends Component {
     render() {
         return (
             <>
-                <section class="pa3 pa5-ns">
-                    <p class="list pl0 measure center">
-                        <span class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30">
+                <section className="pa3 pa5-ns">
+                    <p className="list pl0 measure center">
+                        <span className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30">
                             {this.props.item.name}
                         </span>
                         <button>Delete Product</button>
