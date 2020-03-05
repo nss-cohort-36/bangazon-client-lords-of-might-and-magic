c52a9ac027212e4930ec9677e190661c8603982d
Sophia Hoffman <sophiahoffman@hotmail.com>
2020-03-05 11:51:45 -0600

    edit customer profile

---
+++ bangazon-client-facing/src/components/customer/CustomerEditForm.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import ApiManager from '../utility/ApiManager'
+import { isAuthenticated } from '../helpers/simpleAuth'
+
+export default function CustomerEditForm(props) {
+
+    const [updatedCustomer, setUpdatedCustomer] = useState(
+        {'address': "",
+        'phone':"",
+        'city':"",
+        'first_name': "",
+        'last_name': "",
+        'email': "",
+        'username': ""
+        }
+    )
+
+    const history = useHistory()
+
+    const getUpdatedCustomer = () => {
+        if (isAuthenticated()) {
+        ApiManager.get('customers')
+        .then(r => {
+            console.log(r)
+            return r
+        })
+        .then((r) => {
+            setUpdatedCustomer({
+                'address': r[0].address,
+                'phone': r[0].phone,
+                'city': r[0].city,
+                'first_name': r[0].user.first_name,
+                'last_name': r[0].user.last_name,
+                'email': r[0].user.email,
+                'username': r[0].user.username
+            })
+        }
+        )}
+    }
+
+    const handleInputChange = evt => {
+        setUpdatedCustomer({
+            ...updatedCustomer,
+            [evt.target.id]: evt.target.value
+        })
+    }
+
+    const handleUpdatedCustomer = evt => {
+        evt.preventDefault()
+
+        const updateCustomer = {
+            last_name: updatedCustomer.last_name,
+            address: updatedCustomer.address,
+            city: updatedCustomer.city,
+            phone: updatedCustomer.phone
+        }
+
+        fetch(`http://localhost:8000/customers/${props.match.params.customerId}`, {
+            "method": "PATCH",
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Token ${sessionStorage.getItem("bangazon_token")}`
+            },
+            "body": JSON.stringify(
+                updateCustomer
+            )
+        })
+        // .then(history.push('/myaccount'))
+        // return
+    }
+
+    useEffect(getUpdatedCustomer, [])
+
+    return (
+      <form className="form-container">
+        <label className="form-label" htmlFor="first_name">First Name</label>
+        <p className="form-desc" id="first_name"> {updatedCustomer.first_name}</p><br />
+        
+        <label className="form-label" htmlFor="last_name">Last Name</label>
+        
+        <p><input className="form-input"
+          onChange={handleInputChange}
+          id = "last_name"
+          type="text" name="last_name"
+          value={updatedCustomer.last_name} 
+          autoFocus required />< br/>
+        </p>
+        <label className="form-label" htmlFor="email">Email</label>
+        <p className="form-desc">{updatedCustomer.email}</p>
+        <label className="form-label" htmlFor="username">Username</label>
+        <p className="form-desc">{updatedCustomer.username}</p>
+        <label className="form-label" htmlFor="address">Address</label>
+        <p><input
+          onChange={handleInputChange}
+          id = "address"    
+          type="text" name="address"
+          value={updatedCustomer.address} 
+          autoFocus required />< br/>
+        </p>
+        <label className="form-label" htmlFor="city">City   </label>
+        <p><input
+          onChange={handleInputChange}
+          id = "city"
+          type="text" name="city"
+          value={updatedCustomer.city} 
+          autoFocus required />< br/>
+        </p>
+        <label className="form-label" htmlFor="phone">Phone   </label>
+        <input
+          onChange={handleInputChange}
+          id = "phone"
+          type="text" name="phone"
+          value={updatedCustomer.phone} 
+          autoFocus required />< br/>
+
+        <button className="form-button" onClick={handleUpdatedCustomer}>Update Customer</button>
+      </form>
+    )
+}
